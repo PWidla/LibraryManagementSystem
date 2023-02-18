32fb01cc2ca81a0d59836c4ad8d2269161ce37a5
PWidla <patrykwidlazb@gmail.com>
2023-02-18 16:49:43 +0100

    Modify view of announcement content

--- Models/Announcement.cs
+++ Models/Announcement.cs
@@ -15,6 +15,7 @@ namespace LibraryManagementSystem.Models
 
         [Column(TypeName = "nvarchar(2000)")]
         [Required(ErrorMessage = "Please enter Content")]
+        [MinLength(20)]
         public string Content { get; set; }
 
         [Display(Name = "Date of creation")]
