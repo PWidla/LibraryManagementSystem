673425c5345c6e71171bc61bc4084c276ff86158
PWidla <patrykwidlazb@gmail.com>
2023-02-18 14:38:23 +0100

    Add all controllers

---
+++ Controllers/AnnouncementsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AnnouncementsController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public AnnouncementsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Announcements
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Announcements.ToListAsync());
+        }
+
+        // GET: Announcements/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Announcements == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            return View(announcement);
+        }
+
+        // GET: Announcements/Create
+        [Authorize(Policy = "RequireAdmin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Announcements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Policy = "RequireAdmin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Content,CreationDate")] Announcement announcement)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(announcement);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Edit/5
+        [Authorize(Policy = "RequireAdmin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Announcements == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements.FindAsync(id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+            return View(announcement);
+        }
+
+        // POST: Announcements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Policy = "RequireAdmin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Content,CreationDate")] Announcement announcement)
+        {
+            if (id != announcement.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(announcement);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnnouncementExists(announcement.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(announcement);
+        }
+
+        // GET: Announcements/Delete/5
+        [Authorize(Policy = "RequireAdmin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Announcements == null)
+            {
+                return NotFound();
+            }
+
+            var announcement = await _context.Announcements
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (announcement == null)
+            {
+                return NotFound();
+            }
+
+            return View(announcement);
+        }
+
+        // POST: Announcements/Delete/5
+        [Authorize(Policy = "RequireAdmin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Announcements == null)
+            {
+                return Problem("Entity set 'LibraryContext.Announcements'  is null.");
+            }
+            var announcement = await _context.Announcements.FindAsync(id);
+            if (announcement != null)
+            {
+                _context.Announcements.Remove(announcement);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnnouncementExists(int id)
+        {
+          return _context.Announcements.Any(e => e.ID == id);
+        }
+    }
+}
