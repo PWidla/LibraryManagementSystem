16ca7b304196ef6d8b524f6c9e8b9a73909f3627
PWidla <patrykwidlazb@gmail.com>
2023-02-19 12:20:58 +0100

    Add frontend validation to books' forms

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿//  Announcement create form validation
+﻿var currentYear = new Date().getFullYear();
+
+//  Announcement create form validation
 $('#announcement-form-create').submit(function (e) {
     e.preventDefault();
     var title = $.trim($('#Title').val());
@@ -105,6 +107,89 @@ $('#genre-form-edit').submit(function (e) {
         contentType: false,
         success: function (data) {
             window.location.href = '/Genres/Index';
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+        }
+    });
+});
+
+
+// Book create form validation
+$('#book-form-create').submit(function (e) {
+    e.preventDefault();
+    var title = $.trim($('#Title').val());
+    var releaseYear = $.trim($('#ReleaseYear').val());
+    var genre = $.trim($('#GenreID').val());
+    var author = $.trim($('#AuthorID').val());
+    var currentYear = new Date().getFullYear();
+    if (title.length == 0) {
+        alert('Please enter title.');
+        return;
+    }
+    if (releaseYear.length == 0 || releaseYear < 1 || releaseYear > currentYear) {
+        alert('Please enter release year (valid values are in range from 0 to ' + currentYear + ')');
+        return;
+    }
+    if (genre.length == 0) {
+        alert('Please choose genre.');
+        return;
+    }
+    if (author.length == 0) {
+        alert('Please choose author.');
+        return;
+    }
+
+    var formData = new FormData(this);
+    $.ajax({
+        url: '/Books/Create',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            window.location.href = '/Books/Index';
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+        }
+    });
+});
+
+// Book edit form validation
+$('#book-form-edit').submit(function (e) {
+    e.preventDefault();
+    var title = $.trim($('#Title').val());
+    var releaseYear = $.trim($('#ReleaseYear').val());
+    var genre = $.trim($('#GenreID').val());
+    var author = $.trim($('#AuthorID').val());
+    var currentYear = new Date().getFullYear();
+    if (title.length == 0) {
+        alert('Please enter title.');
+        return;
+    }
+    if (releaseYear.length == 0 || releaseYear < 1 || releaseYear > currentYear) {
+        alert('Please enter release year (valid values are in range from 0 to ' + currentYear + ')');
+        return;
+    }
+    if (genre.length == 0) {
+        alert('Please choose genre.');
+        return;
+    }
+    if (author.length == 0) {
+        alert('Please choose author.');
+        return;
+    }
+
+    var formData = new FormData(this);
+    $.ajax({
+        url: '/Books/Edit',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            window.location.href = '/Books/Index';
         },
         error: function (xhr, status, error) {
             console.log(error);
